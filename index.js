86df2a074869d67ba5d5f10fb5376d83b36f864f
Giorgio Sironi <g.sironi@elifesciences.org>
2025-01-07 13:47:19 +0000

    Respond with the same message on any endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('node:http');
 
 const server = http.createServer((req, res) => {
+  res.write('<!DOCTYPE html><html><head><title>Hello world</title></head><body><h1>Hello world</h1></body></html>');
   res.end();
 });
 
